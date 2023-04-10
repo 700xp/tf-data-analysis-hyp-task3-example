c1f09c0c1461ff23f91354e3b4d7772abdec09c7
Pavel Gogolev <122290372+700xp@users.noreply.github.com>
2023-04-10 23:03:48 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import numpy as np
 
+from scipy.stats import cramervonmises_2samp
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id =  689327667# Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+
+SGN_LVL = 0.01
+
+def solution(x: np.array, y: np.array) -> bool:
+    pval = scipy.stats.cramervonmises_2samp(x,y).pvalue
+    if pval < SGN_LVL:
+        return True
+    # true: выборки не равны
+    return False
