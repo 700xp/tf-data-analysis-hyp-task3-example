3a9bed0bc335f062cd83a7bd928736256d4f3323
Pavel Gogolev <122290372+700xp@users.noreply.github.com>
2023-04-11 20:45:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+from scipy.stats import mannwhitneyu
 
 chat_id = 689327667 # Ваш chat ID, не меняйте название переменной
 
 SGN_LVL = 0.01
 
 def solution(x: np.array, y: np.array) -> bool:
-    pval = ttest_ind(x, y, equal_var=False, alternative="less").pvalue
+    pval = mannwhitneyu(x, y, alternative="less").pvalue
     if pval < SGN_LVL:
         return True
     # true: выборки не равны
