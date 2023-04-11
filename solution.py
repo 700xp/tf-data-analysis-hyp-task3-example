1a50cb4d03ab763ada4b38118d2de9b65b91230f
Pavel Gogolev <122290372+700xp@users.noreply.github.com>
2023-04-11 20:36:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
-from scipy.stats import cramervonmises_2samp
-
-chat_id =  689327667# Ваш chat ID, не меняйте название переменной
-
+chat_id = 689327667 # Ваш chat ID, не меняйте название переменной
 
 SGN_LVL = 0.01
 
 def solution(x: np.array, y: np.array) -> bool:
-    pval = cramervonmises_2samp(x,y).pvalue
+    pval = ttest_ind(x, y, equal_var=False, alternative="less").pvalue
     if pval < SGN_LVL:
         return True
     # true: выборки не равны
